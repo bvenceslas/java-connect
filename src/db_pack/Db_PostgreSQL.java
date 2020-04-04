e4788b90d35f5abfcfcb0c9a446a2016f40b05b1
Venceslas Burongu <bvenceslas@gmail.com>
2020-04-04 22:00:18 +0200

    Adding PostgreSQL ConnectionString

---
+++ src/db_pack/Db_PostgreSQL.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db_pack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Brain
+ */
+public class Db_PostgreSQL {
+    static String username = "root";
+    static String password = "root_brain";
+    static String database = "db_brain";
+    static String url = "jdbc:postgresql://localhost/"+database;
+    
+    private static Connection conn = null;
+    public static Connection connectDbMysql() throws SQLException, ClassNotFoundException, Exception
+    {
+        Class.forName("org.postgresql.Driver");
+        return conn = DriverManager.getConnection(url, username, password);
+    }
+}
